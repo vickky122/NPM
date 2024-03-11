d489a6917a4b44b3211b954490c67ac08a098a6a
Vikrant Yadav <vikrantmani45@gmail.com>
2024-03-11 23:16:09 +0530

    Update script.js

--- test-currency-converter/script.js
+++ test-currency-converter/script.js
@@ -1,3 +1,6 @@
 import { convertCurrency } from "kl-currency-converter";
 
 convertCurrency("USD", "INR", 5).then(res => console.log(res));
+import { convertCurrency } from "kl-currency-converter";
+
+convertCurrency("USD", "INR", 5).then(res => console.log(res));
